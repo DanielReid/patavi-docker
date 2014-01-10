6419872559006f7bd4441058c205d8fdf7649d84
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2014-01-10 13:42:37 +0100

    Clean-up / tuning

--- base/patavi-worker.sh
+++ base/patavi-worker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+export BROKER_SOCKET=$PATAVI_SERVER_PORT_7740_TCP
+
 cd /var/lib/patavi/src/worker
 /var/lib/patavi/bin/lein with-profile production run "$@"
--- run-patavi.sh
+++ run-patavi.sh
@@ -1,2 +1,2 @@
-PATAVI_SERVER=$(docker run -d -p 3000:3000 -p 7740:7740 patavi/server)
-PATAVI_WORKER=$(docker run -d patavi/worker)
+PATAVI_SERVER=$(docker run -d -name patavi_server -p 3000:3000 patavi/server)
+PATAVI_WORKER=$(docker run -d -link patavi_server:patavi_server patavi/worker)
