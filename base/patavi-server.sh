84e6a727d6f5cf4ba37c18270bddcd5795297ec3
box003 deploy <deploy@box003.drugis.org>
2014-07-02 14:31:57 +0200

    Use cached server version by default

--- base/patavi-server.sh
+++ base/patavi-server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-cd /var/lib/patavi/src/server
+export PATAVI_TASK_SILENCE_TIMEOUT=20000
+
+cd /var/lib/patavi/src/server-cached
 echo /var/lib/patavi/bin/lein with-profile production run "$@"
 /var/lib/patavi/bin/lein with-profile production run "$@"
