fe164d42a2c7a09d139d3ff54f8420e3643fb72e
box003 deploy <deploy@box003.drugis.org>
2014-07-03 10:21:05 +0200

    Update docker file with server-cached; update submodule; remove redundant export

--- base/patavi-server.sh
+++ base/patavi-server.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export PATAVI_TASK_SILENCE_TIMEOUT=20000
-
 cd /var/lib/patavi/src/server-cached
 echo /var/lib/patavi/bin/lein with-profile production run "$@"
 /var/lib/patavi/bin/lein with-profile production run "$@"
