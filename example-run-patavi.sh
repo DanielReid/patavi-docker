c74edfc1a03842e62f159f07600ea9a9df19db1c
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-06-13 11:53:37 -0400

    Get Patavi to build again

--- example-run-patavi.sh
+++ example-run-patavi.sh
@@ -1,2 +1,6 @@
-PATAVI_SERVER=$(docker run -d -name patavi_server -p 3000:3000 patavi/server)
-PATAVI_WORKER=$(docker run -d -link patavi_server:patavi_server patavi/slow)
+PATAVI_SERVER=$(docker run -d \
+	-e PATAVI_TASK_SILENCE_TIMEOUT=20000 \
+	-e PATAVI_TASK_GLOBAL_TIMEOUT=300000 \
+	-e PATAVI_CACHE_DB_URL="postgresql://localhost/addiscore?user=addiscore&password=develop" \
+	--name patavi_server -p 3000:3000 patavi/server)
+PATAVI_WORKER=$(docker run -d --link patavi_server:patavi_server patavi/slow)
